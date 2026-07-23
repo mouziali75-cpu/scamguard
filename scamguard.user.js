72649ad71aa121d80403a346cfcb50cf0b7d4ac6
mouziali75-cpu <mouziali75@gmail.com>
2026-07-23 12:56:24 +0100

    Update scamguard.user.js

--- scamguard.user.js
+++ scamguard.user.js
@@ -476,7 +476,7 @@
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         embeds: [{
-          title: currentLang === 'ar' ? '✅ تم اتمام المهام من قبل' : (currentLang === 'fr' ? '✅ Tâche terminée par' : '✅ Task completed by'),
+          title: '✅ Task completed by',
           description: `<@${discordId}>`,
           color: 5025616,
           footer: { text: `User ID: ${discordId}` },
